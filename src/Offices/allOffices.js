8facdd1e94fdb503c2df88e70d7a24833a14af66
klobukov <klobukov.vladimir@rambler.ru>
2021-02-15 20:06:33 +0300

    fix tal office params

--- src/Offices/allOffices.js
+++ src/Offices/allOffices.js
@@ -17,7 +17,7 @@ var mk1 = {
 	workDays: "Понедельник - Пятница",
 	samplingBioMaterialTime: "08:00 - 12:00",
 	resultsTime: "12:00 - 17:00",
-	phone: "(+7978)043-94-47",
+	phone: "+7(978)043-94-47",
 	locationForLink: "https://www.google.ru/maps/place/4+%D0%BF%D0%BB.+%D0%92%D0%BE%D1%81%D1%81%D1%82%D0%B0%D0%B2%D1%88%D0%B8%D1%85,+%D0%A1%D0%B5%D0%B2%D0%B0%D1%81%D1%82%D0%BE%D0%BF%D0%BE%D0%BB%D1%8C/@44.6009303,33.5127546,17z/data=!3m1!4b1!4m5!3m4!1s0x409524b0a58fc373:0x7ddc6ede6fab73a!8m2!3d44.6009265!4d33.5149433",
 	locationForFrame: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2840.8090757571795!2d33.51275461485824!3d44.600930298648926!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x409524b0a58fc373%3A0x7ddc6ede6fab73a!2zNCDQv9C7LiDQktC-0YHRgdGC0LDQstGI0LjRhSwg0KHQtdCy0LDRgdGC0L7Qv9C-0LvRjA!5e0!3m2!1sru!2sru!4v1494597021454"
 }
@@ -27,9 +27,9 @@ let stal = {
 	header: "Манипуляционный кабинет №2",
 	address: 'ул. Героев Сталинграда 43, пансионат "Севастополь" (вход справа)',
 	workDays: "Понедельник - Пятница",
-	samplingBioMaterialTime: "08:00 - 12:00",
-	resultsTime: "12:00 - 17:00",
-	phone: "(8692)55-61-92",
+	samplingBioMaterialTime: "08:30 - 11:30",
+	resultsTime: "08:30 - 15:00",
+	phone: "+7(978)681-70-20",
 	locationForLink: "https://www.google.com/maps/place/Sevastopol/@44.591583,33.444468,17z/data=!4m5!3m4!1s0x0:0xb3e44550aab46607!8m2!3d44.591743!4d33.44362?hl=ru",
 	locationForFrame: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1689.4308193086963!2d33.44405022268329!3d44.59144881011781!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40953c95a9438a35%3A0xb3e44550aab46607!2sSevastopol!5e0!3m2!1sen!2s!4v1612878512009!5m2!1sen!2s"
 }
