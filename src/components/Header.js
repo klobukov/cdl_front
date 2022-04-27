507597ce0d77d33c138aac54f88f806c79b4912a
klobukov <klobukov.vladimir@rambler.ru>
2022-04-27 10:24:06 +0300

    temp may schedule

--- src/components/Header.js
+++ src/components/Header.js
@@ -42,6 +42,16 @@ export default class Header extends React.Component{
 				<div className="header__slideShow">
 					<Carousel />
 				</div>
+				<div style={{"border": "5px solid blue", "padding": "10px", "margin-bottom": "10px"}}>
+					<div style={{"font-size": "20px", "text-align": "center", "margin-bottom": "10px"}}>
+						Поздравляем вас с наступающими майскими праздниками!
+						Пусть эти праздничные дни принесут в ваш дом мир, солнце, радость, хорошее настроение и светлые надежды!
+					</div>
+					<div style={{"font-size": "20px", "text-align": "center"}}>
+						График работы Медицинского центра «Зеленая Волна» ООО «Регион-Стандарт» на майские, праздничные дни:<br/> 
+						<span style={{"font-weight": "bold"}}> 1, 2, 3, 7, 8, 9, 10 мая -  выходные дни. 4, 5, 6  мая –  рабочие дни.</span>
+					</div>
+				</div>
 				<div className="header__covid_container">
 					<img src={covidFull} alt="Covid" />
 				</div>
