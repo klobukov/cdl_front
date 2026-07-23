a7c3d3d2d420044c0563572e92c3c48524ee3acc
klobukov <piratess3295@gmail.com>
2026-07-23 09:31:12 +0300

    raw. readme, docker, fixes

--- backend/admin_connect.php
+++ backend/admin_connect.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__ . '/config/env.php';
-require_once __DIR__ . '/db.php';
+require_once(__DIR__ . '/config/db.php');
 
 $conn = new mysqli ($db_host, $db_user, $db_pass, $db_name);
 if(!$conn) {
--- backend/config/db.php
+++ backend/config/db.php
@@ -1,9 +1,9 @@
 <?php
 
 // use prod params. dev params in comments.
-$db_host = 'localhost'; // "localhost"
+$db_host = 'mysql'; // "mysql"
 $db_user = 'root'; // 'root'
-$db_pass = ''; // ''
+$db_pass = 'root'; // 'root'
 $db_name = 'cdl'; // 'cdl'
 
 ?>
